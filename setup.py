1717d072761fea02f8f568542af4ab99d6f68a4a
Peter Penchev <openstack-dev@storpool.com>
2015-04-15 13:04:14 +0300

    Version bump to python-storpool-1.0.5.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class BuildPyCommand(build_py.build_py):
 
 setuptools.setup(
 	name = 'storpool',
-	version = '1.0.4',
+	version = '1.0.5',
 	packages = ('storpool',),
 	namespace_packages = ('storpool',),
 
