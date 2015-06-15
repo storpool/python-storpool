c5f05c784717c446f60e414a8bcdfd558c7c1bf7
Peter Penchev <openstack-dev@storpool.com>
2015-06-15 18:54:16 +0300

    Prepare for the release of python-storpool-1.3.0.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class BuildPyCommand(build_py.build_py):
 
 setuptools.setup(
 	name = 'storpool',
-	version = '1.2.0',
+	version = '1.3.0',
 	packages = ('storpool',),
 	namespace_packages = ('storpool',),
 
