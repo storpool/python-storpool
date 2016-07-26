45a6bcfcce44c2bd4ecb2c03473207c2984502cc
Peter Penchev <openstack-dev@storpool.com>
2016-07-26 14:55:04 +0300

    This is about to be version 3.0.1.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class BuildPyCommand(build_py.build_py):
 
 setuptools.setup(
 	name = 'storpool',
-	version = '3.0.0',
+	version = '3.0.1',
 	packages = ('storpool',),
 	namespace_packages = ('storpool',),
 
