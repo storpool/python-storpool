3bcd4e66a5b927402a1462b8b8dc017d65e8be30
Peter Pentchev <pp@storpool.com>
2018-12-20 15:19:01 +0200

    python-api: spdoc: fix the copyright years.
    Change-Id: I7cf659b5eb246cbd5b1d955332f176f8d737072d

--- storpool/spdoc.py
+++ storpool/spdoc.py
@@ -2,7 +2,7 @@
 Helper classes for creating the HTML documetation of the StorPool API.
 """
 #
-# Copyright (c) 2014, 2015  StorPool.
+# Copyright (c) 2014 - 2018  StorPool.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
