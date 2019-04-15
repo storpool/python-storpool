65c005ec5966deac8d480490b1f113d700f13078
Peter Pentchev <pp@storpool.com>
2019-04-15 14:56:23 +0300

    python-api: sptest: Python 3 compatibility.
    Change-Id: I07af570310ca5e86534601fff95da3f299df3918

--- scripts/sptest.py
+++ scripts/sptest.py
@@ -31,17 +31,20 @@ collide with any pre-existing setup.
  - Missing most BLOCK client related commands
  - Missing balancer commit
 """
-import argparse
 
-from time import sleep
-from storpool.spapi import Api
+from __future__ import print_function
+
+import argparse
+import time
+
+from storpool import spapi
 
 
 SP_TEST_PREFIX = "sptest_"
 sp_name       = lambda fmt, *args, **kwargs: SP_TEST_PREFIX + fmt.format(*args, **kwargs)
 sp_name_check = lambda name: name.startswith(SP_TEST_PREFIX)
 
-api = Api.fromConfig()
+api = spapi.Api.fromConfig()
 
 reassignIndex = 0
 reassignHandler = [
@@ -59,198 +62,198 @@ def reassign(data):
 	
 def showState():
 	ss = api.servicesList()
-	print "CLUSTER STATUS:", ss.clusterStatus
-	print "SERVERS:", [sId for sId in ss.servers]
-	print "CLIENTS:", [cId for cId in ss.clients]
-	print "MGMT:", [mId for mId in ss.mgmt]
-	print "DISKS:", [diskId for diskId in api.disksList()]
-	print "PLACEMENT GROUPS:", [pgName for pgName in api.placementGroupsList()]
-	print "TEMPLATES:", [t.name for t in api.volumeTemplatesList()]
-	print "VOLUMES:", [v.name for v in api.volumesList()]
-	print "SNAPSHOTS:", [s.name for s in api.snapshotsList()]
+	print("CLUSTER STATUS:", ss.clusterStatus)
+	print("SERVERS:", [sId for sId in ss.servers])
+	print("CLIENTS:", [cId for cId in ss.clients])
+	print("MGMT:", [mId for mId in ss.mgmt])
+	print("DISKS:", [diskId for diskId in api.disksList()])
+	print("PLACEMENT GROUPS:", [pgName for pgName in api.placementGroupsList()])
+	print("TEMPLATES:", [t.name for t in api.volumeTemplatesList()])
+	print("VOLUMES:", [v.name for v in api.volumesList()])
+	print("SNAPSHOTS:", [s.name for s in api.snapshotsList()])
 
 def cleanup():
 	for vol in api.volumesList():
 		if not sp_name_check(vol.name):
 			continue
 		
-		print '- detach and remove volume {name}'.format(name=vol.name)
-		print reassign([{ 'volume': vol.name, 'detach': "all" }])
-		print api.volumeDelete(vol.name)
+		print('- detach and remove volume {name}'.format(name=vol.name))
+		print(reassign([{ 'volume': vol.name, 'detach': "all" }]))
+		print(api.volumeDelete(vol.name))
 	
 	for snap in api.snapshotsList():
 		if not sp_name_check(snap.name):
 			continue
 		
-		print '- detach and remove snapshot {name}'.format(name=snap.name)
+		print('- detach and remove snapshot {name}'.format(name=snap.name))
 		if not snap.autoName:
-			print reassign([{ 'snapshot': snap.name, 'detach': "all" }])
+			print(reassign([{ 'snapshot': snap.name, 'detach': "all" }]))
 		
-		print api.snapshotDelete(snap.name)
+		print(api.snapshotDelete(snap.name))
 	
 	while True:
 		snaps = [snap.name for snap in api.snapshotsList() if snap.deleted]
 		if not snaps:
 			break
 		
-		print "waiting for", snaps
-		sleep(1)
+		print("waiting for", snaps)
+		time.sleep(1)
 	
 	for tmpl in api.volumeTemplatesList():
 		if not sp_name_check(tmpl.name):
 			continue
 		
-		print '- remove template {name}'.format(name=tmpl.name)
-		print api.volumeTemplateDelete(tmpl.name)
+		print('- remove template {name}'.format(name=tmpl.name))
+		print(api.volumeTemplateDelete(tmpl.name))
 	
 	for pgName in api.placementGroupsList():
 		if not sp_name_check(pgName):
 			continue
 		
-		print '- remove placement group {name}'.format(name=pgName)
-		print api.placementGroupDelete(pgName)
+		print('- remove placement group {name}'.format(name=pgName))
+		print(api.placementGroupDelete(pgName))
 	
 	showState()
 
 def setup(no_ssd=False):
-	print '- disksList()'
+	print('- disksList()')
 	disks = api.disksList().values()
-	print '- placementGroup create hdd'
-	print api.placementGroupUpdate(sp_name("hdd"), { 'addDisks': [disk.id for disk in disks if not disk.ssd] })
+	print('- placementGroup create hdd')
+	print(api.placementGroupUpdate(sp_name("hdd"), { 'addDisks': [disk.id for disk in disks if not disk.ssd] }))
 	if no_ssd:
-		print '- placementGroup create ssd with all the disks'
-		print api.placementGroupUpdate(sp_name("ssd"), { 'addDisks': [disk.id for disk in disks] })
+		print('- placementGroup create ssd with all the disks')
+		print(api.placementGroupUpdate(sp_name("ssd"), { 'addDisks': [disk.id for disk in disks] }))
 	else:
-		print '- placementGroup create ssd'
-		print api.placementGroupUpdate(sp_name("ssd"), { 'addDisks': [disk.id for disk in disks if disk.ssd] })
+		print('- placementGroup create ssd')
+		print(api.placementGroupUpdate(sp_name("ssd"), { 'addDisks': [disk.id for disk in disks if disk.ssd] }))
 	
-	print '- template create 1'
-	print api.volumeTemplateCreate({ 'name': sp_name("tmpl_1"), 'size': 10 * 1024 ** 3, 'replication': 1, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") })
-	print '- template create 2'
-	print api.volumeTemplateCreate({ 'name': sp_name("tmpl_2"), 'size': 10 * 1024 ** 3, 'replication': 2, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") })
-	print '- template create 3'
-	print api.volumeTemplateCreate({ 'name': sp_name("tmpl_3"), 'size': 10 * 1024 ** 3, 'replication': 3, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") })
+	print('- template create 1')
+	print(api.volumeTemplateCreate({ 'name': sp_name("tmpl_1"), 'size': 10 * 1024 ** 3, 'replication': 1, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") }))
+	print('- template create 2')
+	print(api.volumeTemplateCreate({ 'name': sp_name("tmpl_2"), 'size': 10 * 1024 ** 3, 'replication': 2, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") }))
+	print('- template create 3')
+	print(api.volumeTemplateCreate({ 'name': sp_name("tmpl_3"), 'size': 10 * 1024 ** 3, 'replication': 3, 'placeAll': sp_name("hdd"), 'placeTail': sp_name("ssd") }))
 	
-	for v in xrange(5):
-		for r in xrange(1, 4):
-			print '- volume {v} template {r}'.format(v=v, r=r)
+	for v in range(5):
+		for r in range(1, 4):
+			print('- volume {v} template {r}'.format(v=v, r=r))
 			volName = sp_name("volume_{v}_{r}", v=v, r=r)
-			print api.volumeCreate({ 'name': volName, 'template': sp_name("tmpl_{r}", r=r) })
-			print reassign([ { 'volume': volName, 'rw': [1] } ])
+			print(api.volumeCreate({ 'name': volName, 'template': sp_name("tmpl_{r}", r=r) }))
+			print(reassign([ { 'volume': volName, 'rw': [1] } ]))
 	
-	print '- volumes list'
+	print('- volumes list')
 	for vol in api.volumesList():
 		if not sp_name_check(vol.name):
 			continue
 		
-		print "Snapshot volume:", vol.name
-		print api.snapshotCreate(vol.name, {})
+		print("Snapshot volume:", vol.name)
+		print(api.snapshotCreate(vol.name, {}))
 		
 		snapName = vol.name + "_snapshot"
-		print api.snapshotCreate(vol.name, { 'name': snapName })
-		print reassign([ { 'snapshot': snapName, 'ro': [1] } ])
+		print(api.snapshotCreate(vol.name, { 'name': snapName }))
+		print(reassign([ { 'snapshot': snapName, 'ro': [1] } ]))
 	
-	print '- volumes list'
+	print('- volumes list')
 	for vol in api.volumesList():
 		if not sp_name_check(vol.name):
 			continue
 		
-		print '  - volume clone {name}'.format(name=vol.name)
-		print api.volumeCreate({ 'name': vol.name + "_clone", 'baseOn': vol.name })
+		print('  - volume clone {name}'.format(name=vol.name))
+		print(api.volumeCreate({ 'name': vol.name + "_clone", 'baseOn': vol.name }))
 	
-	print "Wait for transient snapshot deletion"
+	print("Wait for transient snapshot deletion")
 	while True:
 		snaps = [snap.name for snap in api.snapshotsList() if snap.transient]
 		if not snaps:
 			break
 		
-		print "waiting for:", snaps
-		sleep(1)
+		print("waiting for:", snaps)
+		time.sleep(1)
 	
-	print '- done with setting things up'
+	print('- done with setting things up')
 	showState()
 
 def config():
 	""" Dump Storpool cluster and policy configuration info """
-	print api.peersList()
-	print api.servicesList()
-	print api.serversListBlocked()
-	print api.tasksList()
-	print api.clientsConfigDump()
+	print(api.peersList())
+	print(api.servicesList())
+	print(api.serversListBlocked())
+	print(api.tasksList())
+	print(api.clientsConfigDump())
 	
 	disks = api.disksList()
-	print disks
+	print(disks)
 	
 	for diskId in disks:
-		print api.diskDescribe(diskId)
-		print api.diskInfo(diskId)
-		print api.diskActiveRequests(diskId)
+		print(api.diskDescribe(diskId))
+		print(api.diskInfo(diskId))
+		print(api.diskActiveRequests(diskId))
 		
-		print api.diskSoftEject(diskId)
-		print api.diskSoftEjectPause(diskId)
-		print api.diskSoftEjectCancel(diskId)
+		print(api.diskSoftEject(diskId))
+		print(api.diskSoftEjectPause(diskId))
+		print(api.diskSoftEjectCancel(diskId))
 	
 	pgs = api.placementGroupsList()
-	print pgs
+	print(pgs)
 	for pgName in pgs:
-		print api.placementGroupDescribe(pgName)
+		print(api.placementGroupDescribe(pgName))
 	
 	ts = api.volumeTemplatesList()
-	print ts
-	print api.volumeTemplatesStatus()
+	print(ts)
+	print(api.volumeTemplatesStatus())
 	for t in ts:
-		print api.volumeTemplateDescribe(t.name)
+		print(api.volumeTemplateDescribe(t.name))
 
 def volumes():
 	""" Dump Storpool Volumes and Snapshots info """
-	print api.volumesStatus()
-	print api.snapshotsSpace()
+	print(api.volumesStatus())
+	print(api.snapshotsSpace())
 	
 	vols = api.volumesList()
-	print vols
+	print(vols)
 	for vol in vols:
-		print vol.name
-		print api.volumeList(vol.name)
-		print api.volumeDescribe(vol.name)
-		print api.volumeInfo(vol.name)
-		print api.volumeListSnapshots(vol.name)
+		print(vol.name)
+		print(api.volumeList(vol.name))
+		print(api.volumeDescribe(vol.name))
+		print(api.volumeInfo(vol.name))
+		print(api.volumeListSnapshots(vol.name))
 	
 	snaps = api.snapshotsList()
-	print snaps
+	print(snaps)
 	for snap in snaps:
-		print snap.name
-		print api.snapshotList(snap.name)
-		print api.snapshotDescribe(snap.name)
-		print api.snapshotInfo(snap.name)
+		print(snap.name)
+		print(api.snapshotList(snap.name))
+		print(api.snapshotDescribe(snap.name))
+		print(api.snapshotInfo(snap.name))
 
 def relocator():
 	""" Dump relocator state info """
-	print api.volumeRelocatorStatus()
-	print api.volumeRelocatorDisks()
+	print(api.volumeRelocatorStatus())
+	print(api.volumeRelocatorDisks())
 	
 	for vol in api.volumesList():
-		print api.volumeRelocatorVolumeDisks(vol.name)
+		print(api.volumeRelocatorVolumeDisks(vol.name))
 	
 	for snap in api.snapshotsList():
-		print api.volumeRelocatorSnapshotDisks(snap.name)
+		print(api.volumeRelocatorSnapshotDisks(snap.name))
 
 def scrubbing():
-	diskId = api.disksList().values()[0].id
+	diskId = list(api.disksList().values())[0].id
 	
-	print api.diskScrubPause(diskId)
+	print(api.diskScrubPause(diskId))
 	assert api.disksList()[diskId].scrubbingPaused
 	
-	print api.diskScrubStart(diskId)
+	print(api.diskScrubStart(diskId))
 	d = api.disksList()[diskId]
 	assert d.scrubbingPaused
 	assert d.scrubbingStartedBefore == d.scrubbingPausedFor
 	
-	print api.diskScrubContinue(diskId)
+	print(api.diskScrubContinue(diskId))
 	assert api.disksList()[diskId].scrubbingPaused == False
 
 def remote():
 	if not api.locationsList()['locations']:
-		print "no remote location. skipping test"
+		print("no remote location. skipping test")
 		return
 	
 	for vol in api.volumesList():
@@ -260,14 +263,14 @@ def remote():
 		api.volumeBackup({'volume':vol.name, 'location':loc.name})
 		break;
 	
-	print api.snapshotsRemoteList()
+	print(api.snapshotsRemoteList())
 	
 	while True:
 		r = [r for r in api.snapshotsRemoteList()['snapshots'] if r.name == vol.name]
 		if r:
 			break;
-		print "waiting for remote", vol.name
-		sleep(1)
+		print("waiting for remote", vol.name)
+		time.sleep(1)
 	
 	rem = r[0]
 	api.snapshotFromRemote({ 'name':"restoredSnapshot", 'remoteId' : rem.remoteId, 'remoteLocation' : loc.name, 'template' : "sptest_tmpl_1"})
@@ -294,7 +297,7 @@ def main():
 			exit('UNKNOWN COMMAND: {}'.format(phase))
 		phases.append(ph)
 	for phase in phases:
-		print 'RUNNING {}'.format(phase.__name__)
+		print('RUNNING {}'.format(phase.__name__))
 		if phase == setup:
 			phase(no_ssd=args.no_ssd)
 		else:
