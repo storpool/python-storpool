0e73ab46e8f1aefe1e7c03e70b33f6090e873e6a
Peter Pentchev <pp@storpool.com>
2022-08-22 16:32:20 +0300

    python-api: update some copyright years.
    Change-Id: I85f70289eb99480570a8be344146d9108e4715cf

--- unit_tests/test_spdoc.py
+++ unit_tests/test_spdoc.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019  StorPool.
+# Copyright (c) 2019, 2022  StorPool.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- unit_tests/test_spreq.py
+++ unit_tests/test_spreq.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019, 2021  StorPool.
+# Copyright (c) 2019, 2021, 2022  StorPool.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
